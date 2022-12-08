666140664984956dcdf13ee01e6f19f2dfec1e88
Andres Anni <andres.anni1985@gmail.com>
2022-12-08 16:13:19 -0300

    El texto escrito en los input se muestra en los span

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const[linea1,setLinea1] =useState("");
+  const[linea2,setLinea2] =useState("");
+
+  const onChangeLinea1= function(evento){
+    setLinea1(evento.target.value);
+  }
+
+  const onChangeLinea2= function(evento){
+    setLinea2(evento.target.value);
+  }
+
+
 
   return (
     <div className="App">
@@ -15,12 +29,12 @@ function App() {
         <option value="smart">Smart guy</option>
       </select><br/>
 
-      <input type = "text" placeholder='First line'></input><br/>
-      <input type = "text" placeholder='Second line'></input><br/>
+      <input onChange={onChangeLinea1} type = "text" placeholder='First line'></input><br/>
+      <input onChange={onChangeLinea2}type = "text" placeholder='Second line'></input><br/>
 
       <div>
-        <span>Linea 1</span>
-        <span>Linea 2</span>
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span>
         <img></img>
 
       </div>
